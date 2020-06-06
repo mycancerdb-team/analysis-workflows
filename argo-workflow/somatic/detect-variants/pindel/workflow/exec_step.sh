37ca88c7b5ea8ff0748c5e379cef76f97177a83c
Andrew Popp <apopp@mycancerdb.com>
2020-06-06 14:53:13 -0400

    added working files

--- argo-workflow/somatic/detect-variants/pindel/workflow/exec_step.sh
+++ argo-workflow/somatic/detect-variants/pindel/workflow/exec_step.sh
@@ -15,11 +15,14 @@ set -o errexit
 for d in ${OUTPUTDIR}/pindel/split-beds/*/; do printf "${d}\n" >> /root/scatter_list.txt ; done
 
 while read i; do
-pushd ${i} && /usr/bin/perl /root/run_step.pl $CANCERBAM $NORMALBAM $INSRTSIZE $CANCERNAME $NORMALNAME -f $REF -c $CHROMO -j "${i}scattered.interval_list" && \
-export WRKDIR=${i} && /root/cat-out.sh "${i}all_D" "${i}all_SI" "${i}all_TD" "${i}all_LI" "${i}all_INV" && popd;
+  echo -n "$CANCERBAM $NORMALBAM $INSRTSIZE $CANCERNAME $NORMALNAME -f $REF -c $CHROMO -j ${i}scattered.interval_list"
+  pushd ${i} && /usr/bin/perl /root/run_step.pl $CANCERBAM $NORMALBAM $INSRTSIZE $CANCERNAME $NORMALNAME -f $REF -c $CHROMO -j "${i}scattered.interval_list"
+  export WRKDIR=${i} && /root/cat-out.sh "${i}all_D" "${i}all_SI" "${i}all_TD" "${i}all_LI" "${i}all_INV" && popd;
 done </root/scatter_list.txt
 
 
 while read i; do
   /bin/cat "${i}per_chromosome_pindel.out" | /bin/grep ChrID /dev/stdin >> "${OUTPUTDIR}/pindel/all_region_pindel.head";
 done </root/scatter_list.txt
+
+:
