38c2f0687ab68565a8d8721b6384139a31b1c847
Andrew Popp <apopp@mycancerdb.com>
2020-07-29 22:13:56 -0400

    pindel concurrent upgrade

--- argo-workflow/somatic/detect-variants/pindel/workflow/exec_step.sh
+++ argo-workflow/somatic/detect-variants/pindel/workflow/exec_step.sh
@@ -13,28 +13,30 @@ set -o errexit
 #ENVVAR OUTPUTDIR
 #ENVVAR WRKNGDIR
 
+
 #enumerate the scattered dirs into a list for parsing
-#for d in ${OUTPUTDIR}/pindel/split-beds/*/; do printf "${d}\n" >> /root/scatter_list.txt ; done
+for d in ${OUTPUTDIR}/pindel/split-beds/*/; do printf "${d}\n" >> /root/scatter_list.txt ; done
 
 # Pindel function used to invoke pindel per scattered dir and associated bed file
-pindel () {
+pwrkflw () {
   echo -n "$NORMALBAM $CANCERBAM $INSRTSIZE $NORMALNAME $CANCERNAME -f $REF -c $CHROMO -j ${1}scattered.bed"
   pushd $1 && /usr/bin/perl /root/run_step.pl $NORMALBAM $CANCERBAM $INSRTSIZE $NORMALNAME $CANCERNAME -f $REF -c $CHROMO -j "${1}scattered.bed"
   export WRKDIR=$1 && /root/cat-out.sh "${1}all_D" "${1}all_SI" "${1}all_TD" "${1}all_LI" "${1}all_INV" && popd;
 }
+export -f pwrkflw
 ##
-pindel $WRKNGDIR
-##
-/bin/cat "${WRKNGDIR}per_chromosome_pindel.out" | /bin/grep ChrID /dev/stdin >> "${OUTPUTDIR}/pindel/all_region_pindel.head"
+#pindel $WRKNGDIR
+###
+#/bin/cat "${WRKNGDIR}per_chromosome_pindel.out" | /bin/grep ChrID /dev/stdin >> "${OUTPUTDIR}/pindel/all_region_pindel.head"
 
-## Invoke pindel function forked and parallelized, wait for all instances to complete before moving on
-#while read p; do
-#  pindel ${p}
-#done </root/scatter_list.txt
-#wait
+# Invoke pindel function forked and parallelized, wait for all instances to complete before moving on
+while read p; do
+  sem -j 10 -k "pwrkflw ${p}"
+done </root/scatter_list.txt
+sem --wait
 
-#while read i; do
-#  /bin/cat "${i}per_chromosome_pindel.out" | /bin/grep ChrID /dev/stdin >> "${OUTPUTDIR}/pindel/all_region_pindel.head";
-#done </root/scatter_list.txt
+while read i; do
+  /bin/cat "${i}per_chromosome_pindel.out" | /bin/grep ChrID /dev/stdin >> "${OUTPUTDIR}/pindel/all_region_pindel.head";
+done </root/scatter_list.txt
 
 :
