dfd81fdabb1360127d322951140aa781dca453fb
Andrew Popp <apopp@mycancerdb.com>
2020-08-11 15:34:44 -0400

    improved pindel workflow

--- argo-workflow/somatic/detect-variants/pindel/workflow/exec_step.sh
+++ argo-workflow/somatic/detect-variants/pindel/workflow/exec_step.sh
@@ -12,15 +12,12 @@ set -o errexit
 #ENVVAR CHROMO
 #ENVVAR OUTPUTDIR
 #ENVVAR WRKNGDIR
-
-
-#enumerate the scattered dirs into a list for parsing
-for d in ${OUTPUTDIR}/pindel/split-beds/*/; do printf "${d}\n" >> /root/scatter_list.txt ; done
+#ENVVAR SCTTRLIST
 
 # Pindel function used to invoke pindel per scattered dir and associated bed file
 pwrkflw () {
   echo -n "$NORMALBAM $CANCERBAM $INSRTSIZE $NORMALNAME $CANCERNAME -f $REF -c $CHROMO -j ${1}scattered.bed"
-  pushd $1 && /usr/bin/perl /root/run_step.pl $NORMALBAM $CANCERBAM $INSRTSIZE $NORMALNAME $CANCERNAME -f $REF -c $CHROMO -j "${1}scattered.bed"
+  pushd $1 && /usr/bin/perl /root/run_step.pl $CANCERBAM $NORMALBAM $INSRTSIZE $CANCERNAME $NORMALNAME -f $REF -c $CHROMO -j "${1}scattered.bed"
   export WRKDIR=$1 && /root/cat-out.sh "${1}all_INV" "${1}all_TD" "${1}all_D" "${1}all_SI"  "${1}all_LI"  && popd;
 }
 export -f pwrkflw
@@ -31,12 +28,14 @@ export -f pwrkflw
 
 # Invoke pindel function forked and parallelized, wait for all instances to complete before moving on
 while read p; do
-  sem -j 20 -k "pwrkflw ${p}"
-done </root/scatter_list.txt
+  sem -j 8 -k "pwrkflw ${p}"
+done <"${SCTTRLIST}"
 sem --wait
 
-while read i; do
-  /bin/cat "${i}per_chromosome_pindel.out" | /bin/grep ChrID /dev/stdin >> "${OUTPUTDIR}/pindel/all_region_pindel.head";
-done </root/scatter_list.txt
+
+### pulled out into own step
+#while read i; do
+#  /bin/cat "${i}per_chromosome_pindel.out" | /bin/grep ChrID /dev/stdin >> "${OUTPUTDIR}/pindel/all_region_pindel.head";
+#done </root/scatter_list.txt
 
 :
