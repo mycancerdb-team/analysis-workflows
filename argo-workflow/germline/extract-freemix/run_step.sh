7946f605ca2fe509d3ea53ec760469bc85c19154
Andrew Popp <apopp@mycancerdb.com>
2020-08-26 16:44:49 -0400

    adding freemix score finder

---
+++ argo-workflow/germline/extract-freemix/run_step.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o pipefail
+set -o errexit
+
+#ENVVAR #VERIFYBAMFILE /data/output/normal_final/normal.VerifyBamId.selfSM
+#ENVVAR #OUTPUTDIR
+
+HEADER=$(awk '{print $7}' ${VERIFYBAMFILE} | sed 2d)
+
+if [[ "$HEADER" == "FREEMIX"]]; then
+  awk '{print $7}' ${VERIFYBAMFILE} | sed 1d >> ${OUTPUTDIR}/normal_final/freemix_score.txt
+  exit 0
+else
+  echo -n "Freemix header not found, found $HEADER instead ... failing"
+  exit 1
+fi
