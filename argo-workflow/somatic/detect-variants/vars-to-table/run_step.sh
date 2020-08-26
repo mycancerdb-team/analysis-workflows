413c99594f46f864d71275ec7b2bea39cf6f58a5
Andrew Popp <apopp@mycancerdb.com>
2020-08-26 14:49:43 -0400

    adding vars to table update

--- argo-workflow/somatic/detect-variants/vars-to-table/run_step.sh
+++ argo-workflow/somatic/detect-variants/vars-to-table/run_step.sh
@@ -8,10 +8,12 @@ set -o errexit
 #ENV VARS -- FIELDS
 #ENV VARS -- GENO_FIELDS
 #ENV VARS -- OUTPUTDIR
+#ENV VARS -- SERVICE detect-variants/final
+#ENV VARS -- ID
 
 F=$(for f in ${FIELDS} ; do printf -- "-F ${f} " ; done)
 GF=$(for gf in ${GENO_FIELDS} ; do printf -- "-GF ${gf} " ; done)
 
 
 /usr/bin/java -Xmx8g -jar /opt/GenomeAnalysisTK.jar -T VariantsToTable -R "${REF}" --variant "${VCF}" ${F} \
-${GF} --allowMissingData -o "${OUTPUTDIR}/detect-variants/final/variants.tsv"
+${GF} --allowMissingData -o "${OUTPUTDIR}/${SERVICE}/${ID}_variants.tsv"
